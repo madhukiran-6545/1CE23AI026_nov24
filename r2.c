90808f015f9bbbf0c449abb4ffad59e47bfc7225
madhukiran-6545 <madhuive57@gmail.com>
2024-11-07 16:20:07 +0530

    updated

--- r2.c
+++ r2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+
 int main()
 {
     char foodType = '\0';
@@ -7,51 +8,58 @@ int main()
     puts("Welcome to Madurai Cafe");
     do
     {
-        puts("V:Veg N:Nonveg, Your choice plz: ");
-        foodType = getc(stdin);
-        foodType = tolower(foodType);
+        puts("V: Veg, N: Nonveg. Your choice please: ");
+        scanf(" %c", &foodType); // Notice the space before %c to consume any extra newline
+        foodType = tolower(foodType); // Convert to lowercase
+
         switch (foodType)
         {
-        case 'v':
-            puts("1:Banne-Dosa 2:Upma 3:Rava-idli \nYour choice? ");
+        case 'v': // Veg menu
+            puts("1: Banne-Dosa 2: Upma 3: Rava-idli \nYour choice? ");
             scanf("%d", &choice);
             switch (choice)
             {
             case 1:
-                puts("Your Tasty dosa Sir");
+                puts("Your tasty dosa, Sir!");
                 break;
             case 2:
-                puts("Your yummy upma Maam");
+                puts("Your yummy upma, Ma'am!");
                 break;
             case 3:
-                puts("Your delicious idli Sir");
+                puts("Your delicious idli, Sir!");
                 break;
             default:
-                puts("We dont serve Grass Sir");
+                puts("We don't serve grass, Sir.");
             }
-            {
-        case 'n':
-            puts("1:Mutton-pulav 2:Chicken-chilly 3:Fish-fry. Your choice plz");
+            break; // Add break to avoid fall-through to Nonveg
+
+        case 'n': // Non-veg menu
+            puts("1: Mutton-pulav 2: Chicken-chilly 3: Fish-fry. Your choice please? ");
             scanf("%d", &choice);
             switch (choice)
             {
             case 1:
-                puts("Your Tasty Hanumantu");
+                puts("Your tasty Hanumantu!");
                 break;
             case 2:
-                puts("Guntur delicious Chicken Maam");
+                puts("Guntur delicious chicken, Ma'am!");
                 break;
             case 3:
-                puts("Your spicy fish fry Maam");
+                puts("Your spicy fish fry, Ma'am!");
                 break;
             default:
-                puts("We dont serve Lizards Maam");
+                puts("We don't serve lizards, Ma'am.");
             }
+            break; // Add break to ensure proper exit from the switch
+
+        default:
+            puts("Invalid choice. Please select either V for Veg or N for Non-veg.");
         }
-        puts("Do you wish to have more? 1:Yes 2:No");
+
+        puts("Do you wish to have more? 1: Yes 2: No");
         scanf("%d", &choice);
-    } while (choice != 1)
-    puts("Thank you Visit again");
+    } while (choice == 1); // Keep looping if the user wants more
+
+    puts("Thank you! Visit again.");
     return 0;
-}
 }
